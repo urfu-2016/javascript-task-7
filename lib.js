b0a7a4eadfd86360698b9a0fb589693738db9f50
Zeace <pern2500@ya.ru>
2016-11-21 22:29:37 +0500

    ну оооочень черновой вариант

--- lib.js
+++ lib.js
@@ -1,4 +1,11 @@
 'use strict';
+function compareName(personA, personB) {
+    if (personA.circle < personB.circle) {
+        return personA.circle > personB.circle;
+    }
+
+    return personA.name > personB.name;
+}
 
 /**
  * Итератор по друзьям
@@ -7,7 +14,82 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+    var friendsList = friends.map(function (entry) {
+        return Object.assign({}, entry);
+    });
+    var a = 0;
+    var b = 0;
+    friendsList.forEach(function (friend) {
+        if (friend.best === true) {
+            friend.circle = 1;
+            a++;
+        }
+    });
+    var circle = 1;
+    while (a !== b) {
+        a = b;
+        aaa(friendsList, circle);
+        b = bbb(friendsList);
+        circle++;
+    }
+
+    this.friends = friendsList.filter(function (friend) {
+        return filter.rightGender(friend.gender);
+    }).sort(compareName);
+}
+
+function aaa(friendsList, circle) {
+    friendsList.forEach(function (friend) {
+        if (friend.circle === circle) {
+            friend.friends.forEach(function (name) {
+                friendsList.forEach(function (friend1) {
+                    if (friend1.name === name && friend1.circle === undefined) {
+                        friend1.circle = circle + 1;
+                    }
+                });
+            });
+        }
+    });
+}
+
+function bbb(friendsList) {
+    var count = 0;
+    friendsList.forEach(function (friend) {
+        if (friend.circle !== undefined) {
+            count++;
+        }
+    });
+
+    return count;
+}
+
+
+Iterator.prototype.done = function () {
+    this.friends = this.friends.filter(function (friend) {
+        return friend.circle !== undefined;
+    });
+
+    return (checkDone(this.friends));
+};
+Iterator.prototype.next = function () {
+    this.friends = this.friends.filter(function (friend) {
+        return friend.circle !== undefined;
+    });
+    if (checkDone(this.friends)) {
+        return null;
+    }
+    delete this.friends[0].circle;
+
+    return this.friends[0];
+};
+
+function checkDone(friends) {
+    var count = friends.length;
+
+    return !(count !== 0);
 }
 
 /**
@@ -19,15 +101,25 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+
+    this.friends = this.friends.filter(function (friend) {
+        return friend.circle <= maxLevel;
+    });
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.rightGender = function () {
+
+        return true;
+    };
 }
 
 /**
@@ -36,8 +128,13 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.rightGender = function (gender) {
+        return gender === 'male';
+    };
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +142,13 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.rightGender = function (gender) {
+        return gender === 'female';
+    };
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
