bbfb41cf24811836618c9e2195e1a4bafed34606
MariaShmakova <shmakovamaria95@gmail.com>
2016-11-22 14:15:14 +0500

    Пока просто проверка правильности алгоритма

--- lib.js
+++ lib.js
@@ -6,9 +6,76 @@
  * @param {Object[]} friends
  * @param {Filter} filter
  */
-function Iterator(friends, filter) {
-    console.info(friends, filter);
+var listAllPersons;
+var usedFriendNames;
+var countCircles;
+
+function compareAlphabetically(person1, person2) {
+    if (person1.name > person2.name) {
+        return 1;
+    }
+    if (person1.name < person2.name) {
+        return -1;
+    }
+
+    return 0;
 }
+
+function foundFirstCircle() {
+    countCircles++;
+
+    return listAllPersons.filter(function (person) {
+        return person.best === true;
+    }).sort(compareAlphabetically);
+}
+
+function foundNextCircle(currentListFriends) {
+    countCircles++;
+    var namesFriends = [];
+    currentListFriends.forEach(function (person) {
+        namesFriends = namesFriends.concat(person.friends);
+    });
+
+    return listAllPersons.filter(function (person) {
+        var usedName = usedFriendNames.indexOf(person.name) !== -1;
+        if (namesFriends.indexOf(person.name) !== -1 && !usedName) {
+            usedFriendNames.push(person.name);
+        }
+
+        return (namesFriends.indexOf(person.name) !== -1 && !usedName);
+    }).sort(compareAlphabetically);
+}
+
+function Iterator(friends, filter) {
+    countCircles = 0;
+    usedFriendNames = [];
+    listAllPersons = friends;
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Incorrect data type Filter');
+    }
+    var listBestFriends = [].concat(foundFirstCircle());
+    usedFriendNames = listBestFriends.map(function (person) {
+        return person.name;
+    });
+    this.listGuests = [].concat(listBestFriends);
+    var currentListFriends = foundNextCircle(listBestFriends);
+    this.listGuests = this.listGuests.concat(currentListFriends);
+
+    while (usedFriendNames.length !== friends.length) {
+        currentListFriends = foundNextCircle(currentListFriends);
+        this.listGuests = this.listGuests.concat(currentListFriends);
+    }
+    this.listGuests = this.listGuests.filter(function (friend) {
+        return filter.filterFriends(friend);
+    });
+    this.listGuests.reverse();
+}
+Iterator.prototype.done = function () {
+    return this.listGuests.length === 0;
+};
+Iterator.prototype.next = function () {
+    return this.listGuests.pop();
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,15 +86,41 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    countCircles = 0;
+    usedFriendNames = [];
+    listAllPersons = friends;
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Incorrect data type Filter');
+    }
+    var listBestFriends = [].concat(foundFirstCircle());
+    usedFriendNames = listBestFriends.map(function (person) {
+        return person.name;
+    });
+    this.listGuests = [].concat(listBestFriends);
+    var currentListFriends = foundNextCircle(listBestFriends);
+    this.listGuests = this.listGuests.concat(currentListFriends);
+
+    while (countCircles !== maxLevel) {
+        currentListFriends = foundNextCircle(currentListFriends);
+        this.listGuests = this.listGuests.concat(currentListFriends);
+    }
+    this.listGuests = this.listGuests.filter(function (friend) {
+        return filter.filterFriends(friend);
+    });
+    this.listGuests.reverse();
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.filter = function () {
+        return true;
+    };
 }
 
 /**
@@ -36,8 +129,12 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.filterFriends = function (friend) {
+        return friend.gender === 'male';
+    };
 }
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +142,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.filterFriends = function (friend) {
+        return friend.gender === 'female';
+    };
 }
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
