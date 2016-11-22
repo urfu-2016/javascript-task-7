76ab3eead3b6f43984e97283fa68d0b8e6c62aba
Smallb <bravilov@mail.ru>
2016-11-22 16:56:47 +0500

    Refactor code

--- lib.js
+++ lib.js
@@ -11,7 +11,8 @@ function Iterator(friends, filter) {
         throw new TypeError('Фильтр не является инстансом объекта Filter');
     }
 
-    this.friendshipLevels = divideIntoFriendshipLevels(friends).map(function (level) {
+    var getFriendByName = friendByNameGetter();
+    this.friendshipLevels = divideIntoFriendshipLevels().map(function (level) {
         return filter.filter(level)
             .sort(function (friend1, friend2) {
                 return friend2.name < friend1.name;
@@ -20,6 +21,68 @@ function Iterator(friends, filter) {
 
     this.levelIndex = 0;
     this.nextIndex = 0;
+
+    /**
+     * Делит друзей на круги (уровни) друзей
+     * @returns {Array} - уровни друзей, где 0 - лучшие, 1 - друзья лучших и т.д.
+     */
+    function divideIntoFriendshipLevels() {
+
+        /* Начинаем с лучших друзей */
+        var queueForAdding = friends.filter(function (friend) {
+            return friend.best;
+        });
+
+        /* Запоминаем, что уже брали их */
+        var viewedFriends = queueForAdding.reduce(function (viewed, friend) {
+            viewed[friend.name] = true;
+
+            return viewed;
+        }, Object.create(null));
+
+        var levels = [];
+        var numberOfFriendsInNextLevel;
+        while ((numberOfFriendsInNextLevel = queueForAdding.length)) {
+            var lastLevelIndex = levels.push([]) - 1;
+            for (var i = 0; i < numberOfFriendsInNextLevel; i++) {
+                var currentFriend = queueForAdding.shift();
+                levels[lastLevelIndex].push(currentFriend);
+                queueForAdding = queueForAdding.concat(
+                    getFriendUnviewedFriends(currentFriend, viewedFriends)
+                );
+            }
+        }
+
+        return levels;
+    }
+
+    function friendByNameGetter() {
+        var friendByName = Object.create(null);
+        friends.forEach(function (friend) {
+            friendByName[friend.name] = friend;
+        });
+
+        return function (name) {
+            return friendByName[name];
+        };
+    }
+
+    /**
+     * Берёт друзей друга, которые ещё не были просмотрены
+     * @param {Object} friend - друг, чьих друзей просматриваем
+     * @param {{String: Boolean}} viewedFriendsNames - имена просмотренных друзей
+     * @returns {Object[]} друзья, которые не были просмотрены ранее
+     */
+    function getFriendUnviewedFriends(friend, viewedFriendsNames) {
+        return friend.friends.reduce(function (unviewedFriends, friendName) {
+            if (!viewedFriendsNames[friendName]) {
+                viewedFriendsNames[friendName] = true;
+                unviewedFriends.push(getFriendByName(friendName));
+            }
+
+            return unviewedFriends;
+        }, []);
+    }
 }
 
 Iterator.prototype.done = function () {
@@ -34,9 +97,9 @@ Iterator.prototype.done = function () {
     this.nextIndex = 0;
     this.levelIndex++;
     while (
-        this.levelIndex < this.friendshipLevels.length &&
-        this.friendshipLevels[this.levelIndex].length === 0
-    ) {
+    this.levelIndex < this.friendshipLevels.length &&
+    this.friendshipLevels[this.levelIndex].length === 0
+        ) {
         this.levelIndex++;
     }
 
@@ -46,74 +109,6 @@ Iterator.prototype.done = function () {
 Iterator.prototype.next = function () {
     return !this.done() ? this.friendshipLevels[this.levelIndex][this.nextIndex++] : null;
 };
-
-/**
- * Делит друзей на круги (уровни) друзей
- * @param {Object[]} friends
- * @returns {Array} - уровни друзей, где 0 - лучшие, 1 - друзья лучших и т.д.
- */
-function divideIntoFriendshipLevels(friends) {
-    var levels = [];
-
-    /* Начинаем с лучших друзей */
-    var queueForAdding = friends.filter(function (friend) {
-        return friend.best;
-    });
-
-    /* Запоминаем, что уже брали их */
-    var viewedFriends = queueForAdding.reduce(function (viewed, friend) {
-        viewed[friend.name] = true;
-
-        return viewed;
-    }, Object.create(null));
-
-    var numberOfFriendsInNextLevel;
-    while ((numberOfFriendsInNextLevel = queueForAdding.length)) {
-        levels.push([]);
-        for (var i = 0; i < numberOfFriendsInNextLevel; i++) {
-            var currentFriend = queueForAdding.shift();
-            levels[levels.length - 1].push(currentFriend);
-            queueForAdding = queueForAdding.concat(
-                getFriendUnviewedFriends(currentFriend, viewedFriends, friends)
-            );
-        }
-    }
-
-    return levels;
-}
-
-/**
- * Берёт друзей друга, которые ещё не были просмотрены
- * @param {Object} friend - друг, чьих друзей просматриваем
- * @param {{String: Boolean}} viewedFriendsNames - имена просмотренных друзей
- * @param {Object[]} friends
- * @returns {Object[]} друзья, которые не были просмотрены ранее
- */
-function getFriendUnviewedFriends(friend, viewedFriendsNames, friends) {
-    return friend.friends.reduce(function (unviewedFriends, friendName) {
-        if (!viewedFriendsNames[friendName]) {
-            viewedFriendsNames[friendName] = true;
-            unviewedFriends.push(findFriendWithName(friends, friendName));
-        }
-
-        return unviewedFriends;
-    }, []);
-}
-
-/**
- * Поиск объекта friend по имени
- * @param {Object[]} friends
- * @param {String} name
- * @returns {Object}
- */
-function findFriendWithName(friends, name) {
-    var result = null;
-    friends.some(function (friend) {
-        return friend.name === name ? ((result = friend), true) : false;
-    });
-
-    return result;
-}
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -137,7 +132,7 @@ LimitedIterator.prototype.constructor = LimitedIterator;
  * @constructor
  */
 function Filter() {
-    this.genderPattern = /.*/;
+    this.genderPattern = /[\s\S]*/;
 }
 Filter.prototype.filter = function (friends) {
     return friends.filter(function (friend) {
