b096a72bfa4ae6eef262d76ebf931c51ed8f06d7
Степан <stepanus97@yandex.ru>
2016-11-24 01:51:38 +0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -7,7 +7,19 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    var result = getSortedByNameAndPriorityFriends(friends);
+    var filteredFriends = applyFilter(result, filter);
+    var current = 0;
+    var last = filteredFriends.length;
+
+    return {
+        next: function () {
+            return (current < last) ? filteredFriends[current++] : null;
+        },
+        done: function () {
+            return !(current < last);
+        }
+    };
 }
 
 /**
@@ -19,7 +31,90 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    var result = getSortedByNameAndPriorityFriends(friends, maxLevel);
+    var filteredFriends = applyFilter(result, filter);
+    var current = 0;
+    var last = filteredFriends.length;
+
+    return {
+        next: function () {
+            return (current < last) ? filteredFriends[current++] : null;
+        },
+        done: function () {
+            return !(current < last);
+        }
+    };
+}
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+
+function getSortedByNameAndPriorityFriends(friends, maxLevel) {
+    var priorityGroups = setPriority(friends, maxLevel);
+    var result = [];
+    for (var i = 0; i < priorityGroups.length; i++) {
+        priorityGroups[i] = priorityGroups[i].sort(comparer);
+        result = result.concat(priorityGroups[i]);
+    }
+
+    return result;
+}
+
+function getNlevelFriends(frend, _frends, NlevelFriends) {
+    for (var k = 0; k < frend.friends.length; k++) {
+        getNlevelFriend(_frends, frend, k, NlevelFriends);
+    }
+}
+
+function getNlevelFriend(_frends, frend, k, NlevelFriends) {
+    for (var l = 0; l < _frends.length; l++) {
+        if (_frends[l].name === frend.friends[k]) {
+            NlevelFriends.push(_frends[l]);
+            _frends.splice(l, 1);
+            break;
+        }
+    }
+}
+
+function comparer(a, b) {
+    return (a.name >= b.name) ? 1 : -1;
+}
+
+function getFirstLevelFriends(resultArray, _friends) {
+    var firstLevelFriends = [];
+    for (var i = 0; i < _friends.length; i++) {
+        if (_friends[i].best === true) {
+            firstLevelFriends.push(_friends[i]);
+            _friends.splice(i, 1);
+            i--;
+        }
+    }
+    resultArray.push(firstLevelFriends);
+}
+
+function setPriority(friends, n) {
+    var _friends = [];
+    copyArrays(friends, _friends);
+    var resultArray = [];
+    getFirstLevelFriends(resultArray, _friends);
+    var j = 0;
+    while (_friends.length !== 0) {
+        var NlevelFriends = [];
+        for (var q = 0; q < resultArray[j].length; q++) {
+            getNlevelFriends(resultArray[j][q], _friends, NlevelFriends);
+        }
+        resultArray.push(NlevelFriends);
+        j++;
+        if (n !== undefined && n <= j + 1) {
+            break;
+        }
+    }
+
+    return resultArray;
+}
+
+function copyArrays(oldArray, newArray) {
+    for (var i = 0; i < oldArray.length; i++) {
+        newArray[i] = oldArray[i];
+    }
 }
 
 /**
@@ -27,7 +122,21 @@ function LimitedIterator(friends, filter, maxLevel) {
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+
+    return {
+        type: 'none'
+    };
+}
+
+function applyFilter(friends, filter) {
+    var filteredFriends = [];
+    for (var i = 0; i < friends.length; i++) {
+        if (friends[i].gender === filter.type) {
+            filteredFriends.push(friends[i]);
+        }
+    }
+
+    return filteredFriends;
 }
 
 /**
@@ -36,8 +145,12 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+
+    return {
+        type: 'male'
+    };
 }
+MaleFilter.prototype = Object.create(Filter.prototype);
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +158,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+
+    return {
+        type: 'female'
+    };
 }
+FemaleFilter.prototype = Object.create(Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
