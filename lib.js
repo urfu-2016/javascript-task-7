cff4b3496823740629b893019bc3940d3b597743
Tsvetkova Valeria <lera.tsvetkova96@gmail.com>
2016-11-24 04:27:42 +0500

    Решение

--- lib.js
+++ lib.js
@@ -7,7 +7,77 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    this.friendsCollection = getFilteredFriends(friends, filter, Infinity);
+    this.currentIndex = 0;
+}
+
+Iterator.prototype.done = function () {
+    return this.friendsCollection.length === this.currentIndex;
+};
+
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+    var person = this.friendsCollection[this.currentIndex];
+    this.currentIndex++;
+
+    return person;
+};
+
+function getFilteredFriends(friends, filter, maxLevel) {
+    if (filter instanceof Filter === false) {
+        throw new TypeError('Incorrect filter!');
+    }
+    var friendsCollection = [];
+    var filterNote = function (friend) {
+        return friendsCollection.indexOf(friend) === -1;
+    };
+    var bestFriends = friends.filter(function (person) {
+        return person.best;
+    }).sort(sortByName);
+    while (bestFriends.length > 0 && maxLevel > 0) {
+        friendsCollection = friendsCollection.concat(bestFriends);
+        bestFriends = getUniqueFriends(bestFriends)
+            .map(function (name) {
+                return friends.filter(function (person) {
+                    return person.name === name;
+                })[0];
+            })
+            .sort(sortByName)
+            .filter(filterNote);
+        maxLevel--;
+    }
+
+    return friendsCollection.filter(filter.filter);
+}
+
+function sortByName(a, b) {
+    if (a.name > b.name) {
+
+        return 1;
+    } else if (a.name < b.name) {
+
+        return -1;
+    }
+
+    return 0;
+}
+
+function getUniqueFriends(collection) {
+    var result = [];
+    collection.forEach(function (element) {
+        var newFriends = element.friendsCollection;
+        if (newFriends.length > 0) {
+            newFriends.forEach(function (friend) {
+                if (result.indexOf(friend) === -1) {
+                    result.push(friend);
+                }
+            });
+        }
+    });
+
+    return result;
 }
 
 /**
@@ -19,15 +89,20 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.currentIndex = 0;
+    this.friendsCollection = getFilteredFriends(friends, filter, maxLevel);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.filter = function () {
+        return true;
+    };
 }
 
 /**
@@ -36,8 +111,12 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.filter = function (person) {
+        return person.gender === 'male';
+    };
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +124,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.filter = function (person) {
+        return person.gender === 'female';
+    };
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
