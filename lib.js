6888fb56a26af69dd03df9058f6cc2ff882dabe0
FairTex <makarov906@gmail.com>
2016-11-23 23:18:15 +0500

    commit

--- lib.js
+++ lib.js
@@ -5,9 +5,71 @@
  * @constructor
  * @param {Object[]} friends
  * @param {Filter} filter
+ * @param {Number} maxLevel
  */
-function Iterator(friends, filter) {
-    console.info(friends, filter);
+function Iterator(friends, filter, maxLevel) {
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+
+    var depth = maxLevel || -1;
+    var book = createBook(friends);
+    var index = 0;
+    var bestFriends = friends.filter(function (friend) {
+        return friend.best;
+    }).map(function (friend) {
+        return friend.name;
+    });
+    var invitedFriends = inviteFriends(bestFriends, book, filter, depth);
+
+    this.next = function () {
+        return this.done() ? null : invitedFriends[index++];
+    };
+
+    this.done = function () {
+        return index >= invitedFriends.length;
+    };
+}
+
+function createBook(friends) {
+    return friends.reduce(function (book, friend) {
+        book[friend.name] = {
+            friends: friend.friends,
+            gender: friend.gender
+        };
+
+        return book;
+    }, {});
+}
+
+function getAllFriends(people, book) {
+
+    return people.reduce(function (allFriends, friend) {
+        return allFriends.concat(book[friend].friends);
+    }, []);
+}
+
+function inviteFriends(bestFriends, book, filter, depth) {
+    var invited = filter.filter(bestFriends, book);
+    invited.sort();
+
+    var allFriends = bestFriends;
+    depth--;
+
+    while (depth !== 0) {
+        allFriends = getAllFriends(allFriends, book);
+        var nextInvitedFriends = filter.filter(allFriends, book);
+        nextInvitedFriends = nextInvitedFriends.filter(function (friend) {
+            return invited.indexOf(friend) === -1;
+        });
+        if (nextInvitedFriends.length === 0) {
+            break;
+        }
+        invited = invited.concat(nextInvitedFriends);
+        depth--;
+    }
+
+    return invited;
 }
 
 /**
@@ -19,15 +81,24 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter, maxLevel);
 }
 
 /**
  * Фильтр друзей
  * @constructor
+ * @param {String} gender
  */
-function Filter() {
-    console.info('Filter');
+function Filter(gender) {
+    this.gender = gender;
+    this.filter = function (invited, book) {
+        var inv = invited.filter(function (friend) {
+            return book[friend].gender === gender;
+        });
+        inv.sort();
+
+        return inv;
+    };
 }
 
 /**
@@ -36,7 +107,7 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    Filter.call(this, 'male');
 }
 
 /**
@@ -45,8 +116,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    Filter.call(this, 'female');
 }
+
+FemaleFilter.prototype = Filter.prototype;
+MaleFilter.prototype = Filter.prototype;
+LimitedIterator.prototype = Iterator.prototype;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
