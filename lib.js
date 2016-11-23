2c6a2705acbd1b35e25e161aba338795fe8c71ac
Koshcheev Nikita <nikitakosheleo@live.ru>
2016-11-23 07:51:42 +0500

    test pass

--- lib.js
+++ lib.js
@@ -1,5 +1,74 @@
 'use strict';
 
+/* function findByName(list, name) {
+    return list.find(function (element) {
+        return element.name === name;
+    });
+}*/
+
+function getCopy(item) {
+    if (item instanceof Array) {
+        return item.map(function (element) {
+            return getCopy(element);
+        });
+    }
+
+    var copy = {};
+
+    return Object.keys(item).reduce(function (obj, key) {
+        obj[key] = item[key];
+
+        return obj;
+    }, copy);
+}
+
+function getWave(friends) {
+    var roster = friends.pop();
+    var vawe = [];
+    var result = [];
+
+    vawe = friends[friends.length - 1].reduce(function (acc, person) {
+        return acc.concat(person.friends);
+    }, vawe);
+
+    roster = roster.filter(function (person) {
+        if (vawe.indexOf(person.name) !== -1) {
+            result.push(person);
+
+            return false;
+        }
+
+        return true;
+    });
+
+    return friends.concat([result, roster]);
+}
+
+
+function sortFriends(friends) {
+    var bestFriends = [];
+
+    friends = friends.filter(function (person) {
+        if (person.best) {
+            bestFriends.push(person);
+
+            return false;
+        }
+
+        return true;
+    });
+
+    friends = [bestFriends, friends];
+
+    while (friends[friends.length - 1].length) {
+        friends = getWave(friends);
+    }
+
+    friends.pop();
+
+    return friends;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,7 +76,22 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    var roster = sortFriends(getCopy(friends));
+
+    this.invited = [];
+    while (roster.length) {
+        this.invited = this.invited.concat(
+                roster.pop().filter(filter.filter)
+            );
+    }
+
+    Iterator.prototype.done = function done() {
+        return this.invited.length === 0;
+    };
+
+    Iterator.prototype.next = function next() {
+        return this.invited.pop();
+    };
 }
 
 /**
@@ -19,7 +103,17 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    var roster = sortFriends(getCopy(friends));
+    roster = roster.splice(0, maxLevel);
+
+    this.invited = [];
+    while (roster.length) {
+        this.invited = this.invited.concat(
+                roster.pop().filter(filter.filter)
+            );
+    }
+
+    Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
 }
 
 /**
@@ -27,7 +121,13 @@ function LimitedIterator(friends, filter, maxLevel) {
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    var flag = true;
+
+    return {
+        filter: function () {
+            return flag;
+        }
+    };
 }
 
 /**
@@ -36,7 +136,13 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    var result = {
+        filter: function filter(person) {
+            return person.gender === 'male';
+        }
+    };
+
+    return Object.setPrototypeOf(result, new Filter());
 }
 
 /**
@@ -45,7 +151,13 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    var result = {
+        filter: function filter(person) {
+            return person.gender === 'female';
+        }
+    };
+
+    return Object.setPrototypeOf(result, new Filter());
 }
 
 exports.Iterator = Iterator;
