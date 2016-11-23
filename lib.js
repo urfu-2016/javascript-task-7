510c4c9fe7d1db3ded331f83681c5b2a38a40527
Antonov Alexey <aleksei193@mail.ru>
2016-11-23 19:04:08 +0500

    Первый вариант

--- lib.js
+++ lib.js
@@ -7,7 +7,96 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('filter не является инстансом функции-конструктора Filter');
+    }
+
+    this.currentFriends = [];
+    this.otherFriends = {};
+    this.filter = filter;
+
+    friends.forEach(function (friend) {
+        if (friend.hasOwnProperty('best') && friend.best === true) {
+            this.currentFriends.push(friend);
+        } else {
+            this.otherFriends[friend.name] = friend;
+        }
+    }, this);
+
+    this.currentFriends.sort(compare);
+    this.nextFriend = this.getNext();
+}
+
+function compare(a, b) {
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    if (a.name < b.name) {
+        return -1;
+    }
+
+    return 0;
+}
+
+Iterator.prototype.getNext = (function () {
+    var friendIndex = 0;
+
+    return function () {
+        if (this.done()) {
+            return null;
+        }
+
+        while (friendIndex < this.currentFriends.length) {
+            var friend = this.currentFriends[friendIndex];
+            friendIndex++;
+
+            if (friendIndex === this.currentFriends.length) {
+                this.currentFriends = getNextLevel(this.otherFriends, this.currentFriends);
+                friendIndex = 0;
+            }
+
+            if (this.filter.isSuitable(friend)) {
+                return friend;
+            }
+        }
+
+        return null;
+    };
+}());
+
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+
+    var oldFriend = this.nextFriend;
+    this.nextFriend = this.getNext();
+
+    return oldFriend;
+};
+
+Iterator.prototype.done = function () {
+    return this.nextFriend === null;
+};
+
+function getNextLevel(otherFriends, level) {
+    var nextLevel = [];
+
+    for (var i = 0; i < level.length; i++) {
+        var currentFriend = level[i];
+        currentFriend.friends.forEach(function (nameFriend) {
+            if (otherFriends.hasOwnProperty(nameFriend) &&
+                nextLevel.indexOf(otherFriends[nameFriend]) === -1) {
+                nextLevel.push(otherFriends[nameFriend]);
+                delete otherFriends[nameFriend];
+            }
+        });
+    }
+
+    nextLevel.sort(compare);
+
+    return nextLevel;
 }
 
 /**
@@ -19,16 +108,69 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.maxLevel = maxLevel;
+    Iterator.call(this, friends, filter);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
+
+LimitedIterator.prototype.getNext = (function () {
+    var friendIndex = 0;
+    var currentLevel = 1;
+
+    return function () {
+        if (this.done()) {
+            return null;
+        }
+
+        while (friendIndex < this.currentFriends.length && currentLevel <= this.maxLevel) {
+            var friend = this.currentFriends[friendIndex];
+            friendIndex++;
+
+            var finish = friendIndex === this.currentFriends.length;
+
+            if (finish && currentLevel < this.maxLevel) {
+                this.currentFriends = getNextLevel(this.otherFriends, this.currentFriends);
+            }
+
+            if (finish) {
+                friendIndex = 0;
+                currentLevel++;
+            }
+
+            if (this.filter.isSuitable(friend)) {
+                return friend;
+            }
+        }
+
+        return null;
+    };
+}());
+
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.nothing = 'nothing';
 }
+
+Filter.prototype.isSuitable = function (item) {
+    return item !== null;
+};
+
+function GenderFilter() {
+    this.gender = 'nothing';
+}
+
+GenderFilter.prototype = Object.create(Filter.prototype);
+GenderFilter.prototype.constructor = GenderFilter;
+
+GenderFilter.prototype.isSuitable = function (item) {
+    return item.hasOwnProperty('gender') && item.gender === this.gender;
+};
 
 /**
  * Фильтр друзей
@@ -36,8 +178,11 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.gender = 'male';
 }
+
+MaleFilter.prototype = Object.create(GenderFilter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +190,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.gender = 'female';
 }
+
+FemaleFilter.prototype = Object.create(GenderFilter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
+
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
