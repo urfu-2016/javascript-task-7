ee1618c4bfa333cdcda0e146312410335596d4de
SvalovaNastya <Sun-7402@mail.ru>
2016-11-24 17:34:40 +0500

    сделана задача

--- lib.js
+++ lib.js
@@ -1,5 +1,64 @@
 'use strict';
 
+function getBestFriends(friends) {
+    return friends.filter(function (item) {
+        return item.best;
+    });
+}
+
+function getNextLevelFriends(lastLevelFriends, friendsWithoutLevel) {
+    var arrays = lastLevelFriends.map(function (item) {
+        return item.friends;
+    });
+    var nextLevelFriendsNames = arrays.length > 1 ? [].concat.apply([], arrays) : arrays[0];
+
+    return friendsWithoutLevel.filter(function (item) {
+        return nextLevelFriendsNames.indexOf(item.name) !== -1;
+    });
+}
+
+function createFriendsWithLevel(friends, level) {
+    return friends.map(function (item) {
+        return { 'friend': item, 'level': level };
+    });
+}
+
+function subtractArrays(arr1, arr2, keyFn) {
+    if (!keyFn) {
+        keyFn = function (item) {
+            return item;
+        };
+    }
+    var arrKeys = arr2.map(function (item) {
+        return keyFn(item);
+    });
+
+    return arr1.filter(function (item) {
+        return arrKeys.indexOf(keyFn(item)) === -1;
+    });
+}
+
+function divideFriendsOnLevels(bestFriends, friends) {
+    var levelsCount = 0;
+    var dividedFriends = createFriendsWithLevel(bestFriends, levelsCount);
+    var friendsOnLastLevel = bestFriends;
+
+    var getName = function (item) {
+        return item.name || item.friend.name;
+    };
+
+    var friendsWithoutLevel = subtractArrays(friends, dividedFriends, getName);
+    while (friendsWithoutLevel.length > 0 && friendsOnLastLevel.length > 0) {
+        levelsCount++;
+        friendsOnLastLevel = getNextLevelFriends(friendsOnLastLevel, friendsWithoutLevel);
+        dividedFriends = [].concat(dividedFriends, createFriendsWithLevel(friendsOnLastLevel,
+            levelsCount));
+        friendsWithoutLevel = subtractArrays(friendsWithoutLevel, friendsOnLastLevel, getName);
+    }
+
+    return dividedFriends;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +66,34 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw TypeError;
+    }
+    this.sortedFriends = [];
+    var bestFriends = getBestFriends(friends);
+    var dividedFriends = divideFriendsOnLevels.call(this, bestFriends, friends);
+
+    var filteredFriends = filter.filter(dividedFriends);
+
+    this.sortedFriends = filteredFriends.sort(function (a, b) {
+        return (a.level > b.level || (a.level === b.level && a.friend.name > b.friend.name))
+            ? 1 : -1;
+    });
+
+    this.index = 0;
 }
+
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+
+    return this.sortedFriends[this.index++].friend;
+};
+
+Iterator.prototype.done = function () {
+    return this.index >= this.sortedFriends.length;
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +104,27 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.sortedFriends = this.sortedFriends.filter(function (item) {
+        return item.level < maxLevel;
+    });
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.fieldValues = ['female', 'male'];
 }
+
+Filter.prototype.filter = function (array) {
+    return array.filter(function (item) {
+        return this.fieldValues.indexOf(item.friend.gender) !== -1;
+    }, this);
+};
 
 /**
  * Фильтр друзей
@@ -36,7 +132,7 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.fieldValues = ['male'];
 }
 
 /**
@@ -45,8 +141,11 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.fieldValues = ['female'];
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype = Object.create(Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
